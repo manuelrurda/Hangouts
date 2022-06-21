4b0643f613ec53c53fbb02b17306023bdfa48db1
manuelrurda <manuelrurda@gmail.com>
2022-06-20 20:55:47 -0700

    Login Fragment

--- app/src/main/java/com/example/hangouts/LoginActivity.java
+++ app/src/main/java/com/example/hangouts/LoginActivity.java
@@ -1,14 +1,43 @@
 package com.example.hangouts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.hangouts.Fragments.LoginFragment;
+import com.example.hangouts.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    private FragmentContainerView fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        fragmentContainer = binding.fragmentContainer;
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final Fragment loginFragment = new LoginFragment();
+        replaceFragment(fragmentManager, fragmentContainer, loginFragment);
+
+    }
+
+    private void replaceFragment(FragmentManager fragmentManager,
+                                 FragmentContainerView fragmentContainer, Fragment fragment) {
+        fragmentManager.beginTransaction()
+                .replace(fragmentContainer.getId(), fragment)
+                .commit();
     }
 }
