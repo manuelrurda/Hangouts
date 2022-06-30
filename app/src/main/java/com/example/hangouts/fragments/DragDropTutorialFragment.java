49eb5a1acb485d8c00e5ac854da46d1b4299e3a7
manuelrurda <manuelrurda@gmail.com>
2022-06-30 15:06:28 -0700

    Tutorial Screen Animations

--- app/src/main/java/com/example/hangouts/fragments/DragDropTutorialFragment.java
+++ app/src/main/java/com/example/hangouts/fragments/DragDropTutorialFragment.java
@@ -2,26 +2,65 @@ package com.example.hangouts.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.hangouts.R;
+import com.example.hangouts.databinding.FragmentDragDropTutorialBinding;
 
 public class DragDropTutorialFragment extends Fragment {
 
+    public static final String TAG = "TUTORIAL";
 
+    private FragmentDragDropTutorialBinding binding;
 
-    public DragDropTutorialFragment() {
-        // Required empty public constructor
-    }
+    private ImageView ivDropZone;
+    private ImageView ivPreferenceCard;
+    private ImageView ivHand;
+
+    public DragDropTutorialFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_drag_drop_tutorial, container, false);
+        binding = FragmentDragDropTutorialBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ivDropZone = binding.ivDropZone;
+        ivPreferenceCard = binding.ivPreferenceCard;
+        ivHand = binding.ivHand;
+
+        moveAnimation(ivPreferenceCard, Animation.ABSOLUTE, -500);
+        moveAnimation(ivHand, Animation.ABSOLUTE, -500);
+
+    }
+
+    private void moveAnimation(View v, int toX, int toY) {
+        Animation animation = new TranslateAnimation(Animation.ABSOLUTE, toX,
+                Animation.ABSOLUTE, toY);
+        animation.setDuration(1000);
+        animation.setFillAfter(true);
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.setRepeatMode(Animation.REVERSE);
+        animation.setInterpolator(new AnticipateInterpolator(1));
+        animation.setStartOffset(500);
+
+        v.startAnimation(animation);
     }
 }
