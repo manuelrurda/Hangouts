138fc053732ffeff0c92fa45afa7ab7862af43f9
manuelrurda <manuelrurda@gmail.com>
2022-07-08 17:09:06 -0700

    Added camera movement when selecting location

--- app/src/main/java/com/example/hangouts/homeScreen/fragments/HangoutLocationSelectionMapFragment.java
+++ app/src/main/java/com/example/hangouts/homeScreen/fragments/HangoutLocationSelectionMapFragment.java
@@ -18,7 +18,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.example.hangouts.BuildConfig;
 import com.example.hangouts.R;
