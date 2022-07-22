daad2ecb39aee1de05982b6d7219e99e2a937cc0
manuelrurda <manuelrurda@gmail.com>
2022-07-21 22:56:25 -0700

    scroll view for recycler views

--- app/src/main/java/com/example/hangouts/homeScreen/HomeFragment.java
+++ app/src/main/java/com/example/hangouts/homeScreen/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.DividerItemDecoration;
