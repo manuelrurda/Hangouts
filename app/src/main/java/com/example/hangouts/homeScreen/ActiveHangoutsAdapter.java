590595ba37b65562ed49922949b4d15a75d0fc10
manuelrurda <manuelrurda@gmail.com>
2022-07-26 11:06:40 -0700

    Close button open dialog

--- app/src/main/java/com/example/hangouts/homeScreen/ActiveHangoutsAdapter.java
+++ app/src/main/java/com/example/hangouts/homeScreen/ActiveHangoutsAdapter.java
@@ -1,7 +1,9 @@
 package com.example.hangouts.homeScreen;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,13 +18,16 @@ import com.example.hangouts.R;
 import com.example.hangouts.databinding.ItemActiveHangoutBinding;
 import com.example.hangouts.homeScreen.utils.DateTimeUtil;
 import com.example.hangouts.models.Hangout;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class ActiveHangoutsAdapter extends RecyclerView.Adapter<ActiveHangoutsAdapter.ViewHolder> {
 
@@ -80,6 +85,30 @@ public class ActiveHangoutsAdapter extends RecyclerView.Adapter<ActiveHangoutsAd
                             .getDateString(deadlineDate));
                     binding.tvItemTime.setText(DateTimeUtil
                             .getTimeString(deadlineDate));
+                    if(Objects.equals(hangout.getHost().getObjectId(), ParseUser.getCurrentUser().getObjectId())){
+                        binding.btnCloseHangout.setVisibility(View.VISIBLE);
+                        binding.btnCloseHangout.setOnClickListener(this::onClickClose);
+                    }
+                }
+
+                private void onClickClose(View view) {
+                    Resources resources = context.getResources();
+                    new MaterialAlertDialogBuilder(context)
+                            .setTitle(resources.getString(R.string.close_hangout_dialog_title))
+                            .setMessage(resources.getString(R.string.close_hangout_dialog_text))
+                            .setNegativeButton(resources.getString(R.string.close_hangout_dialog_cancel),
+                                    this::onClickCancel)
+                            .setPositiveButton(resources.getString(R.string.close_hangout_dialog_accept),
+                                    this::onClickAccept)
+                            .show();
+                }
+
+                private void onClickAccept(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(context, "Accepted", Toast.LENGTH_SHORT).show();
+                }
+
+                private void onClickCancel(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(context, "Canceled", Toast.LENGTH_SHORT).show();
                 }
             });
         }
